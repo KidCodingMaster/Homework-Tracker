b172d12428df2190218b7147887cdfa6a5229fa8
KidCodingMaster <lenovotab12101@gmail.com>
2025-05-29 21:43:50 +0400

    Add student functionality with signup and login support

--- main.py
+++ main.py
@@ -17,6 +17,14 @@ class Teacher:
         print("You are logged in as a teacher")
 
 
+class Student:
+    def __init__(self, name):
+        self.name = name
+
+    def run(self):
+        print("You are logged in as a student")
+
+
 def generate_password():
     characters = list(string.punctuation + string.ascii_letters + string.digits)
     random.shuffle(characters)
@@ -34,11 +42,11 @@ def signup(username, person_type):
     password = generate_password()
 
     with shelve.open(settings["usernameFile"]) as db:
-        if username in db:
+        if username in db and db[username]["type"] == person_type:
             print("You already have an account!")
             return
 
-        db[username] = password
+        db[username] = {"password": password, "type": person_type}
 
     print(f"Your password is {password}")
 
@@ -46,29 +54,44 @@ def signup(username, person_type):
 
     if person_type == "teacher":
         person = Teacher(username)
+    elif person_type == "student":
+        person = Student(username)
 
-        person.run()
+        with shelve.open(settings["studentsFile"]) as db:
+            db[username] = {"tasks": []}
+
+    person.run()
 
 
 def login(username, password, person_type):
     with shelve.open(settings["usernameFile"]) as db:
         if username in db:
-            if db[username] == password:
+            if (
+                db[username]["password"] == password
+                and db[username]["type"] == person_type
+            ):
                 print("Login Successful!")
 
                 if person_type == "teacher":
                     person = Teacher(username)
+                elif person_type == "student":
+                    person = Student(username)
 
-                    person.run()
-            else:
+                person.run()
+            elif db[username]["password"] != password:
                 print("Incorrect password!")
+            elif db[username]["type"] != password:
+                print("Incorrect type!")
         else:
             print("Username not found!")
 
 
 settings = load_settings()
 
-person = input("Are you a teacher or a student? ")
+person = input("Are you a teacher or a student? ").lower()
+
+if person not in ("teacher", "student"):
+    print("Invalid! You can only login or signup as a teacher or a student!")
 
 login_type = input("Do you want to login or signup? ").lower()
 
